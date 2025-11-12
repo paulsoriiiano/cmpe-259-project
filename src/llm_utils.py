257f6ae7e166e95b78ce70269aab5019660baf79
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2025-11-12 15:56:05 -0800

    Update llm_utils.py to use API-based LLMs.

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -1,18 +1,26 @@
 """
-Loads different LLMs for the project.
+Loads different API-based LLMs for the project.
 """
 
-from langchain import HuggingFacePipeline
-from transformers import pipeline
+from langchain_huggingface import HuggingFaceEndpoint
 
-def load_model(size="small"):
+def load_model(api_token, size="small"):
     if size == "small":
-        model_name = "mistralai/Mistral-7B-v0.1"
+        model_name = "mistralai/Mistral-7B-v0.2"
     elif size == "large":
         model_name = "meta-llama/Llama-2-13b-chat-hf"
     else:
         print("I am not aware of this model.")
         return
     
-    pipe = pipeline("text-generation", model=model_name, max_new_tokens=512)
-    return HuggingFacePipeline(pipeline=pipe)
+    # Create API-based LLM 
+    llm = HuggingFaceEndpoint(
+        repo_id = model_name,
+        task = "text-generation",
+        temperature = 0.7,
+        max_new_tokens = 512,
+        repetition_penalty = 1.1,
+        huggingfacehub_api_token=api_token
+    )
+
+    return llm
