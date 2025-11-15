1f2e91bbd595288b21dd1090a36cdeca1c10d0d4
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2025-11-14 23:20:55 -0800

    Added function to load models using langchain_huggingface.

--- src/llm_utils.py
+++ src/llm_utils.py
@@ -2,10 +2,15 @@
 Loads different API-based LLMs for the project.
 """
 
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_mistralai import ChatMistralAI
 from langchain_openai import ChatOpenAI
 
+
 def load_model(api_token, size="small"):
+    """ Loads a Chat model using specific Chats. """
+    
     if size == "small":
         llm = ChatMistralAI(
           model = "mistral-tiny-2312",
@@ -26,3 +31,31 @@ def load_model(api_token, size="small"):
         return
 
     return llm
+
+
+def load_chat_model(size="small"):
+    """ Loads a Chat model using HuggingFaceEndpoint and ChatHuggingFace. """
+
+    if size == "small":
+        llm = HuggingFaceEndpoint(
+          repo_id = "mistralai/Mistral-7B-Instruct-v0.2",
+          task = "text-generation",
+          max_new_tokens = 512,
+          temperature = 0.7,
+          repetition_penalty = 1.03,
+          provider = "auto"
+        )
+    elif size == "large":
+        llm = HuggingFaceEndpoint(
+          repo_id = "meta-llama/Llama-3.3-70B-Instruct",
+          task = "text-generation",
+          max_new_tokens = 512,
+          temperature = 0.7,
+          repetition_penalty = 1.03,
+          provider = "auto"
+        )
+    else:
+        print("I am not aware of this model.")
+        return
+
+    return ChatHuggingFace(llm=llm)
