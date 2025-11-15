867e489e9c0559d1f0b8979d48efe584ebb65537
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2025-11-14 18:08:41 -0800

    Updated to use Runnables.

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -2,21 +2,36 @@
 This module provides querying functions.
 """
 
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 
-def two_step_query(query: str, llm, retriever):
-  """ Perform a two-step RAG chain. """
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+
+def build_rag_chain(llm, retriever):
+  """ Build a two-step RAG chain. """
 
   # 1. Retrieve documents
-  rag_docs = retriever.get_relevant_documents(query)
-  rag_context = "\n\n".join([d.page_content for d in rag_docs])
+  def retrieve_and_format(query):
+
+    rag_docs = retriever.get_relevant_documents(query)
+    rag_context = "\n\n".join([d.page_content for d in rag_docs])
+    rag_sources = "\n\n".join([f"{d.metadata["title"]} information from {d.metadata["url"]}" for d in rag_docs])
+
+    inputs = {
+        "context": rag_context,
+        "sources": rag_sources,
+        "question": RunnablePassthrough()
+    }
+
+    return inputs
 
   # 2. Create prompt
-  prompt = PromptTemplate.from_template("""
+  prompt = ChatPromptTemplate.from_template("""
   You are a helpful virtual assistant for California parks and outdoors. 
   Use the provided context from park data to answer the question.
   If information is missing, respond with the best estimate or say "I'm not sure."
+  Always cite your sources, whenever possible.
 
   Context:
   {context}
@@ -25,15 +40,20 @@ def two_step_query(query: str, llm, retriever):
   Question:
   {question}
 
+                                        
   Answer:
+                                        
 
-  
+  Sources:
+  {sources}                                      
   """)
 
-  rag_chain = LLMChain(llm=llm, prompt=prompt)
-  answer = rag_chain.run({
-      "context": rag_context,
-      "question": query
-  })
+  # 3. Build chahin
+  rag_chain = (
+    RunnableLambda(retrieve_and_format)
+    | prompt
+    | llm
+    | StrOutputParser()
+  )
 
-  return answer
+  return rag_chain
