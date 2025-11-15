e7868c63edf87dc6a65a5c5ffd2aa82b3b41045f
paulsoriiiano <53189056+paulsoriiiano@users.noreply.github.com>
2025-11-14 18:51:20 -0800

    Trial 2.

--- src/rag_pipeline.py
+++ src/rag_pipeline.py
@@ -3,7 +3,7 @@ This module provides querying functions.
 """
 
 
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
@@ -12,22 +12,21 @@ def build_rag_chain(llm, retriever):
   """ Build a two-step RAG chain. """
 
   # 1. Retrieve documents
-  def retrieve_and_format(inputs):
-    query = inputs["input"]
+  def retrieve_and_format(query):
     rag_docs = retriever.invoke(query)
     rag_context = "\n\n".join([d.page_content for d in rag_docs])
     rag_sources = "\n\n".join([f"{d.metadata["title"]} information from {d.metadata["url"]}" for d in rag_docs])
 
     inputs = {
         "context": rag_context,
-        "input": query,
+        "input": RunnablePassthrough(),
         "sources": rag_sources,
     }
 
     return inputs
 
   # 2. Create prompt
-  prompt = PromptTemplate.from_template("""
+  prompt = ChatPromptTemplate.from_template("""
   You are a helpful virtual assistant for California parks and outdoors. 
   Use the provided context from park data to answer the question.
   If information is missing, respond with the best estimate or say "I'm not sure."
